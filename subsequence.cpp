67057959a5ece582b55aa3d139770c543b2fe899
Vasav <vasav2002@gmail.com>
2021-10-08 23:15:25 +0530

    Subarray and Subsequence

---
+++ subsequence.cpp
@@ -0,0 +1,39 @@
+/* Subsequence : A subsequence is a sequence that can be dervied from an array by selecting zero or more elements, without changing the order of the remaning elements */
+
+/* Number of subsequences of an array of size n = 2 raised to the power n */
+
+/* Every subarray is a subsequence but every subsequnce is not a subarray */
+
+/* C++ code to generate all possible subsequences.
+	Time Complexity O(n * 2^n) */
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSubsequences(int arr[], int n)
+{
+	/* Number of subsequences is (2**n -1)*/
+	unsigned int opsize = pow(2, n);
+
+	/* Run from counter 000..1 to 111..1*/
+	for (int counter = 1; counter < opsize; counter++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			/* Check if jth bit in the counter is set
+				If set then print jth element from arr[] */
+			if (counter & (1<<j))
+				cout << arr[j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+// Driver program
+int main()
+{
+	int arr[] = {1, 2, 3, 4};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout << "All Non-empty Subsequences\n";
+	printSubsequences(arr, n);
+	return 0;
+}
