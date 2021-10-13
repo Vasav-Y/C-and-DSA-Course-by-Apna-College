09742dffb9d67c97e22dfb784e2b5a0240f94440
Vasav <vasav2002@gmail.com>
2021-10-13 23:10:52 +0530

    Array

--- character_array.cpp
+++ character_array.cpp
@@ -15,36 +15,3 @@
 // 
 // 	return 0;
 // }
-
-
-// C++ program to illustrate the traversal
-// of the array
-#include "iostream"
-using namespace std;
-
-// Function to illustrate traversal in arr[]
-void traverseArray(int arr[], int N)
-{
-
-	// Iterate from [1, N-1] and print
-	// the element at that index
-	for (int i = 0; i < N; i++) {
-		cout << arr[i] << ' ';
-	}
-}
-
-// Driver Code
-int main()
-{
-
-	// Given array
-	int arr[] = { 1, 2, 3, 4 };
-
-	// Size of the array
-	int N = sizeof(arr) / sizeof(arr[0]);
-
-	// Function call
-	traverseArray(arr, N);
-}
-/* This is how we can traverse a character array */
-/* will surely start making good contributions from tommorow*/
