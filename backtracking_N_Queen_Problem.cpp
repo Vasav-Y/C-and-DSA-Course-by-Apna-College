37f61e94196af8a18d81d25f619bae91852c45f8
Vasav <vasav2002@gmail.com>
2021-10-30 22:21:51 +0530

    Backtracking

---
+++ backtracking_N_Queen_Problem.cpp
@@ -0,0 +1,96 @@
+/*
+We've been given an nxn chessboard and we've to place n queens on it in such a way that no two queens attack each other
+*/
+#include <iostream>
+using namespace std;
+
+bool isSafe(int **arr, int x, int y, int n)
+{
+    // checking for a col in each row whether it has a queen or not
+    for (int row = 0; row < x; row++)
+    {
+        if (arr[row][y] == 1)
+        {
+            return false;
+        }
+    }
+
+    // checking upper left diagonal
+    int row = x;
+    int col = y;
+    while (row >= 0 && col >= 0)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col--;
+    }
+
+    //checking upper right diagonal
+    row = x;
+    col = y;
+    while (row >= 0 && col < n)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col++;
+    }
+    return true;
+}
+
+bool nQueen(int **arr, int x, int n)
+{
+    if (x >= n)
+    {
+        return true;
+    }
+
+    for (int col = 0; col < n; col++)
+    {
+        if (isSafe(arr, x, col, n))
+        {
+            arr[x][col] = 1;
+
+            if (nQueen(arr, x + 1, n))
+            {
+                return true;
+            }
+
+            arr[x][col] = 0; //backtracking
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+    if (nQueen(arr, 0, n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
