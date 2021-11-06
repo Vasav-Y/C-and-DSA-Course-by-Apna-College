dfca5cc6b61139ffb567141001e8649a76fa2ec5
Vasav <vasav2002@gmail.com>
2021-11-06 22:42:24 +0530

    Sieve Of Eratosthenes

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,78 @@
+/// * Finding the prime numbers
+// #include<iostream>
+// using namespace std;
+// 
+// void primeSieve(int n)
+// {
+//     int prime[100] = {0};
+// 
+//     for(int i =2; i<=n; i++)
+//     {
+//         if(prime[i]==0)
+//         {
+//             for(int j=i*i; j<=n; j+=i)
+//             {
+//                 prime[j] = 1;
+//             }
+//         }
+//     }
+//     for(int i =2; i<=n; i++)
+//     {
+//         if(prime[i] == 0)
+//         {
+//             cout<<i<<" ";
+//         }
+//     }
+// }
+// 
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     primeSieve(n);
+// 
+//     return 0;
+// }
+
+//* Finding Prime factors
+
+#include<iostream>
+using namespace std;
+
+void primeFactor(int n)
+{
+    int spf[100] = {0};
+    for(int i =2; i<=n; i++)
+    {
+        spf[i] = i;
+    }
+
+    for(int i =2; i<=n; i++)
+    {
+        if(spf[i] == i)
+        {
+            for(int j = i*i; j<=n; j+=i)
+            {
+                if(spf[j]== j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+
+    while(n!=1)
+    {
+        cout<<spf[n]<<" ";
+        n = n/spf[n];
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    primeFactor(n);
+
+    return 0;
+}
